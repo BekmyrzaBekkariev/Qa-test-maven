08c94e326882bc3685588e29f8899b92141c1fa6
bekmyrza <bekmyrzabekkariev05@gmail.com>
2024-02-18 19:19:04 +0100

    good

--- src/main/java/lessons/practiceone.java
+++ src/main/java/lessons/practiceone.java
@@ -1,16 +1,27 @@
 package lessons;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class practiceone {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/home/bekmyrza/Downloads/chromedriver-linux64/chromedriver");
-
-
         WebDriver driver = new ChromeDriver();
-
+        driver.manage().window().maximize();
         driver.get("https://google.com");
+
+        // переменная
+
+        WebElement accept = driver.findElement(By.xpath("//*[@id='W0wltc']/div"));
+        accept.click();
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+        input.sendKeys("google");
+        input.sendKeys(Keys.ENTER);
+
+
     }
 }
 
