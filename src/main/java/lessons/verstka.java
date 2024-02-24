ae85e72b47640e81c7f4ab143dcaca67c2d2670d
bekmyrza <bekmyrzabekkariev05@gmail.com>
2024-02-24 21:45:26 +0100

    good

--- src/main/java/lessons/verstka.java
+++ src/main/java/lessons/verstka.java
@@ -5,22 +5,38 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+import java.time.Duration;
 
 public class verstka {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/home/bekmyrza/Downloads/chromedriver-linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://google.com");
+        driver.manage().timeouts().implicitlyWait((Duration.ofSeconds(10)));
+        try {
+            driver.get("https://jqueryui.com/droppable/");
+            Thread.sleep(2000);
 
-        // переменная
+            WebElement element = driver.findElement(By.xpath(""));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-        WebElement accept = driver.findElement(By.xpath("//*[@id='W0wltc']/div"));
-        accept.click();
-        WebElement input = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
-        input.sendKeys("google");
-        input.sendKeys(Keys.ENTER);
 
+        // Action
+//        Actions actions = new Actions();
+
+        //        // - с этими можно манипулировать элементами
+        //        actions.moveToElement()// -
+        //                .keyDown()// -  нажимаем на кнопку
+        //                .keyUp() // - как будто мы отжимаем клик
+        //                .clickAndHold() // - нажать и задержать
+        //                .release() // -  отпусть
+        //                .build() // - что бы все это нужно собрать
+        //                .perform(); // - для применения
 
     }
 }
